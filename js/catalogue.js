670e6ff83caf831e611704f87678ba035017351c
sultonakobirov <zafarzhanovich@gmail.com>
2023-05-23 10:32:21 +0500

    "in process"

--- js/catalogue.js
+++ js/catalogue.js
@@ -9,7 +9,7 @@ document.querySelector('section').addEventListener('mouseover', ()=>{
     document.querySelector('.inner-list').style.display = 'none'
     document.querySelector('.search-input').style.display = 'none'
     document.querySelector('.search-input').value = ''
-    document.querySelector('.search-img').src = './images/search.svg'
+    document.querySelector('.search-img').src = '..//images/search.svg'
     document.querySelector('.tel-number').style.display = 'block'
     document.querySelector('.search-img').style.position = 'unset'
     
@@ -17,26 +17,23 @@ document.querySelector('section').addEventListener('mouseover', ()=>{
 document.querySelector('.search-img').addEventListener('mouseover', ()=>{
     document.querySelector('.tel-number').style.display = 'none'
     document.querySelector('.search-block').style.gap = '0'
-    document.querySelector('.search-img').src = './images/search-red.png'
+    document.querySelector('.search-img').src = '..//images/search-red.png'
     document.querySelector('.search-img').style.position = 'absolute'
     document.querySelector('.search-input').style.display = 'block'
 })
 document.querySelector('.search-img').addEventListener('click', ()=>{
     document.querySelector('.search-input').value = ''
 })
-document.querySelector('.next-custom').addEventListener('mouseover', ()=>{
-    document.querySelector('.next').src = './images/hover-next.svg'
-    document.querySelector('.next').style.rotate = '0deg'
-})
-document.querySelector('.next-custom').addEventListener('mouseout', ()=>{
-    document.querySelector('.next').src = './images/next-button.png'
-    document.querySelector('.next').style.rotate = '180deg'
-})
-document.querySelector('.prev-custom').addEventListener('mouseover', ()=>{
-    document.querySelector('.prev').src = './images/hover-next.svg'
-    document.querySelector('.prev').style.rotate = '180deg'
-})
-document.querySelector('.prev-custom').addEventListener('mouseout', ()=>{
-    document.querySelector('.prev').src = './images/next-button.png'
-    document.querySelector('.prev').style.rotate = '0deg'
-})
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
